fffc23345ed34ac1458d711017efcd237d708786
siavashprh <siavash.prh@gmail.com>
2023-04-15 16:35:06 +0330

    initial commit.

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+
+'''
+the Player class.
+the Player class has the id, token, balance, position and properties attributes.
+the id and token attributes are self-explanatory.
+the balance attribute represents the amount of money the player has. which is set to 1500 at the start of the game.
+the position attribute represents the locatio nof the player on the board.
+the properties attributes is the list of properties owned by the player. it is a list containing objects of Property class.
+the pay and receive methods decrease and increase the player balance respectively.
+'''
+class Player:
+    def __init__(self, id, token, balance=1500, position=0, properties=[]):
+        self.id = id
+        self.token = token
+        self.balance = balance
+        self.position = position
+        self.properties = properties
+
+    def __str__(self):
+        return f"Player {self.id} ({self.token}): balance=${self.balance}, position={self.position}, properties={self.properties}"
+        
+    def pay(self, amount):
+        self.balance -= amount
+    
+    def receive(self, amount):
+        self.balance += amount
+
+class Board:
+    def __init__(self, players, properties):
+        self.players = players
+        self.properties = properties
+    
+    def __str__(self):
+        pass
+
+    def move_player(self, player, steps):
+        pass
+
+class Dice:
+    def __init__(self):
+        pass
+
+    def roll(self):
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        return dice1 + dice2
+
+class Property:
+    def __init__(self, name, value, owner):
+        self.name = name
+        self.value = value
+        self.owner = owner
+
+class Game:
+    def __init__(self, players, properties):
+        self.board = Board(players, properties)
+        self.dice = Dice()
